cde6bc2649cab00a06145630d3d62de927ce84b0
luanvmendes <luanvmendes2@gmail.com>
2020-02-21 14:52:18 -0300

    Finalizado

--- DTO/EventoDTO.cs
+++ DTO/EventoDTO.cs
@@ -8,16 +8,13 @@ namespace CasaShow.DTO
     {
         [Required]
         public int Id { get; set; }
-        [Required(ErrorMessage = "Nome da categoria é obrigatório")]
-        [StringLength(50, ErrorMessage = "Categoria não pode exceder 50 caracteres")]
-        [MinLength(3, ErrorMessage = "Categoria precisa ter ao menos 3 caracteres")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [MinLength(3, ErrorMessage = "Campo precisa ter ao menos 3 caracteres")]
         public string Nome { get; set; }
         [Required(ErrorMessage = "Informe a lotação do evento")]
         public int Capacidade { get; set; }
-        [Required(ErrorMessage = "Campo obrigatório")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm}")]
         public DateTime Data { get; set; }
-        [Required(ErrorMessage = "Campo obrigatório")]
         [Display(Name = "Preço do Ingresso")]
         //[DataType(DataType.Currency)]
         [DisplayFormat(DataFormatString = "R$ {0:N2}")]
